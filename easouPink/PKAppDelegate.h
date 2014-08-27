247ace2f1ab01a23609f4b7a3f1162e8c8fba8ac
Neo <Neal@NealdeMacBook-Pro.local>
2014-08-27 18:45:47 +0800

    Initial Commit

---
+++ easouPink/PKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PKAppDelegate.h
+//  easouPink
+//
+//  Created by Neo on 14-8-27.
+//  Copyright (c) 2014年 Neo Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ easouPink/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  easouPink
+//
+//  Created by Neo on 14-8-27.
+//  Copyright (c) 2014年 Neo Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PKAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PKAppDelegate class]));
+    }
+}
